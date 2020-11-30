9f8b3569ac6bed0bdc11d4bbf0599185a29e80cd
ycchang4 <ycchang4@email.txt>
2020-11-30 11:40:11 +0800

    attempt to implement BFS for finding the shortest path

--- traversalUtil.cpp
+++ traversalUtil.cpp
@@ -22,3 +22,23 @@ void BFS::bfs(Vertex source, Vertex destination) {
 
     }
 }
+
+/** find the shortest path between vertices
+ * graph - the airline graph
+ * source = the starting point
+ * aid - the airport id 
+ */
+
+
+void BFS::shortestPath(const Graph &graph, int source, string aid) {
+    return;
+    // distBwVertices.resize(std::numeric_limits<double>::infinity());
+    // distBwVertices[source] = 0;
+    // //push the starting point to the priority queue
+    // pq.push(vertex(source, 0));
+    // while(!pq.empty()) {
+    //     int index = pq.top().index;
+    //     pq.pop();
+    //     for()
+    // }
+}
--- traversalUtil.h
+++ traversalUtil.h
@@ -12,12 +12,21 @@
 #include <list>
 #include <queue>
 
+struct vertex {
+    int index;
+    double weight;
+    vertex(int i = int(), double w = double()) : index(i), weight(w) {}
 
+};
 class BFS {
     public:
 
         //BFS algorithm, add the airport for the traversal to visit
         void bfs(Vertex source, Vertex destination);
+
+
+        //find the shortest path between all the vertices(airports)
+        void shortestPath(const Graph &graph, int source, string aid);
 
 
     private:
@@ -26,5 +35,10 @@ class BFS {
 
         //stores the visited location
         bool *visited;
+
+        //stores the distance between two vertices
+        vector<double> distBwVertices;
+
+        std::priority_queue<vertex> pq;
         
 };
