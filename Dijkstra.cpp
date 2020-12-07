361027826914096ea230cd5bf9f91e6e392b73bc
Jiale Ning <jialen2@linux-a2.ews.illinois.edu>
2020-12-07 13:13:43 -0600

    add untracked

---
+++ Dijkstra.cpp
@@ -0,0 +1,65 @@
+#include "Dijkstra.h"
+Dijkstra::Dijkstra(AirGraph airgraph, const Vertex &src) {
+    graph = airgraph.getGraph();
+    source = src;
+    buildMap();
+}
+void Dijkstra::buildMap() {
+    Node* start = new Node(source, 0, Vertex());
+    store[source] = start;
+    pq_.push(start);
+    while (!pq_.empty()) {
+        Node* curr = pq_.pop();
+        vector<Vertex> adja = graph.getAdjacent(curr->curr);
+        for (unsigned i = 0; i < adja.size(); i++) {
+            // if the Vectex has been visited before
+            if (visited.find(adja[i]) != visited.end()) {
+                continue;
+            }
+            double dist = getDistance(curr->curr, adja[i]) + curr->dist;
+            auto next = store.find(adja[i]);
+            // if haven't calculate distance of the Vertex before
+            if (next == store.end()) {
+                Node* new_node = new Node(adja[i], dist, curr->curr);
+                pq_.push(new_node);
+                store[adja[i]] = new_node;
+            } else {
+                // if smaller distance found
+                if (next->second->dist > dist) {
+                    next->second->dist = dist;
+                    next->second->pre = curr->curr;
+                }
+            }
+        }
+        // set current Vertex visited
+        visited[curr->curr] = true;
+    }
+}
+
+double Dijkstra::distanceTo(Vertex &dest) {
+    // if given destination is unreachable
+    if (visited.find(dest) == visited.end()) {
+        return -1.0;
+    }
+    return store.find(dest)->second->dist;
+}
+
+vector<Edge> Dijkstra::shortestPathTo(const Vertex &dest) {
+    vector<Edge> toReturn;
+    if (visited.find(dest) == visited.end()) {
+        return toReturn;
+    }
+    Vertex curr = dest;
+    while (curr != source) {
+        Node* curr_node = store.find(curr)->second;
+        Vertex prev = curr_node->pre;
+        toReturn.insert(toReturn.begin(), graph.getEdge(prev, curr));
+        curr = prev;
+    }
+    return toReturn;
+}
+Dijkstra::~Dijkstra() {
+    for (auto i = store.begin(); i != store.end(); i++) {
+        delete i->second;
+    }
+}
---
+++ Node.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "vertex.h"
+using namespace std;
+class Node {
+    public:
+        Vertex curr = Vertex();
+        Vertex pre = Vertex();
+        double dist = 0;
+        Node(Vertex given1, double given2, Vertex given3) {
+            curr = given1;
+            dist = given2;
+            pre = given3;
+        }
+        Node() {}
+};
